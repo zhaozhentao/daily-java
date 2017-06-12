7e4c0bde582f2476d4a9fb657a49760bc1f78ac9
zhaozhentao <344696734@qq.com>
2017-06-12 21:20:13 +0800

    create project

---
+++ src/main/java/com/zzt/daily/Application.java
@@ -0,0 +1,14 @@
+package com.zzt.daily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zhaotao on 2017/6/12.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
