8ed4bde5eb3ffe2adfcfc6a29cef7ec743ae5d54
zhaozhentao <344696734@qq.com>
2017-06-12 22:22:02 +0800

    extends

--- src/main/java/com/zzt/daily/controller/PagesController.java
+++ src/main/java/com/zzt/daily/controller/PagesController.java
@@ -1,7 +1,16 @@
 package com.zzt.daily.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * Created by zhaotao on 2017/6/12.
  */
+@Controller
 public class PagesController {
+
+    @GetMapping("/")
+    public String home() {
+        return "pages/index";
+    }
 }
