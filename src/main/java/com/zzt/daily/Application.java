26c564271f35d3656505cf764587c28f57f21d12
zhaozhentao <344696734@qq.com>
2017-06-17 21:48:48 +0800

    tomcat application

--- src/main/java/com/zzt/daily/Application.java
+++ src/main/java/com/zzt/daily/Application.java
@@ -2,6 +2,8 @@ package com.zzt.daily;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
@@ -9,9 +11,14 @@ import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
  * Created by zhaotao on 2017/6/12.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 
     @Bean()
