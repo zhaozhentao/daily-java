e154eea204e7ceef496844eb0568e9c42117b260
zhaozhentao <344696734@qq.com>
2017-06-18 08:54:59 +0800

    删除static views相关代码

--- src/main/java/com/zzt/daily/Application.java
+++ src/main/java/com/zzt/daily/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 /**
  * Created by zhaotao on 2017/6/12.
@@ -21,15 +20,4 @@ public class Application extends SpringBootServletInitializer {
         return application.sources(Application.class);
     }
 
-    @Bean()
-    public ServletContextTemplateResolver templateResolver() {
-        final ServletContextTemplateResolver resolver =
-            new ServletContextTemplateResolver();
-        resolver.setPrefix("/WEB-INF/templates/");
-        resolver.setSuffix(".html");
-        resolver.setTemplateMode("HTML5");
-        resolver.setCacheable(false);
-        resolver.setCharacterEncoding("UTF-8");
-        return resolver;
-    }
 }
